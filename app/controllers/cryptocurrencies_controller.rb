79fb781671a3ee418e6a9e6889a56a84f7725d64
Ruben Dias <rubenmsd@hotmail.com>
2018-12-11 16:56:45 +0000

    Fixed my miostake of creating cryptocurrencies controller Deleted cryptocurrenciesController and routes, changed my view to the right folder, and fixed background colors

--- app/controllers/cryptocurrencies_controller.rb
+++
@@ -1,9 +0,0 @@
-class CryptocurrenciesController < ApplicationController
-  def index
-    @cryptos = Cryptocurrency.all
-  end
-
-  def show
-    @crypto = Cryptocurrency.find(params[:id])
-  end
-end
--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,14 +1,14 @@
 class TradesController < ApplicationController
   def index
     # this is an array of instances
-    @cryptocurrencies = Cryptocurrency.all
+    @cryptos = Cryptocurrency.all
     # this is an hash of hashes with current prices
     @live_prices = CryptoCompareService.new.call_current_prices
     @live_news = CryptoCompareService.new.call_latest_news
   end
 
   def show
-
+    @crypto = Cryptocurrency.find(params[:id])
   end
 
   def new
