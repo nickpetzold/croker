9a4bea2897fa7d961abc0403a5b1231b5d49a950
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-11 20:18:17 +0000

    MORE DYNAMISM FOR DASHBOARD
    ADDED DYNAMIC REFRESH TO TOP5 PULLING FROM AN API INSTANCE METHOD
    
    ADDED DYNAMIC REFRESH TO NEWS PULLING FROM AN API INSTANCE METHOD
    
    ADDED ZEC ICON TO ASSET FOLDER
    
    MADE THE ACCORDING METHODS IN DASHBOARD CONTROLLER TRIGERRING
    BEFORE ACTION SO WE LEAVE THE DASHBOARD METHOD FOR DASHBOARD
    SPECIFIC TASKS ONLY

--- app/services/crypto_compare_service.rb
+++ app/services/crypto_compare_service.rb
@@ -26,7 +26,8 @@ class CryptoCompareService
     Waves: "WAVES",
     Zcash: "ZEC",
     Siacoin: "SC",
-    Augur: "REP" }
+    Augur: "REP"
+  }
 
   FIAT_CURRENCIES = "USD"
 
@@ -41,17 +42,11 @@ class CryptoCompareService
     @base_url_historical_prices = "https://min-api.cryptocompare.com/data/histoday"
     # base url for get requests for latest news
     @base_url_current_news = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
+    # base url for get requests for top10 traded
+    # be careful because the minium number for request is 10 with a max 100
+    # since we only need the top5 i left the limit=10 in the base url
+    @base_url_top_traded = "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10"
     @api_key = ENV['CRYPTO_COMPARE_API_KEY']
-  end
-
-  def hash_to_string_and_array
-    # Iterate through the crypto_hash
-    CRYPTO_HASH.each do |key, value|
-      # Build a string with all cryptocurrency tickers "BTC,ETH"
-      @crypto_string_with_tickers << "#{value},"
-      # Build an array with all cryptocurrency names ["Bitcoin", "Ethereum"]
-      @crypto_array_with_names << key.to_s
-    end
   end
 
   def call_current_prices
@@ -104,5 +99,33 @@ class CryptoCompareService
     query_params = {api_key: @api_key}
     response = RestClient.get(@base_url_current_news, {params: query_params})
     JSON.parse(response)["Data"]
+  end
+
+  def call_top5_traded
+    query_params = {
+      tsym: FIAT_CURRENCIES,
+      api_key: @api_key
+    }
+
+    crypto_top_5_hash = {}
+    response = RestClient.get(@base_url_top_traded, {params: query_params})
+    JSON.parse(response)["Data"].first(5).each do |crypto|
+      crypto_top_5_hash[crypto["CoinInfo"]["FullName"]] = crypto["CoinInfo"]["Name"]
+    end
+    # this will return something like this
+    # {:Bitcoin=>"BTC", :Ethereum=>"ETH", :EOS=>"EOS", :XRP=>"XRP", :ZCash=>"ZEC"}
+    crypto_top_5_hash
+  end
+
+  private
+
+  def hash_to_string_and_array
+    # Iterate through the crypto_hash
+    CRYPTO_HASH.each do |key, value|
+      # Build a string with all cryptocurrency tickers "BTC,ETH"
+      @crypto_string_with_tickers << "#{value},"
+      # Build an array with all cryptocurrency names ["Bitcoin", "Ethereum"]
+      @crypto_array_with_names << key.to_s
+    end
   end
 end
