22e80bd22021ce6e69174b201eda8c7807e1eb90
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-11 22:00:46 +0000

    ADDED MEMOIZATION TO DASHBOARD CONTROLLERS FOR API REQUESTS REFACTORED NEWS LINKS ON DASHBOARD TO REDIRECT TO A NEW TAB INSTEAD OF LEAVING CROKER/DASHBOARD

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,15 +5,21 @@ class DashboardsController < ApplicationController
 
   end
 
+  private
+
   def top_five
     # this returns an hash with the top5 most traded by volume
     # across all the different exchanges
     # Also, it comes sorted from the API
     # ex: {:Bitcoin=>"BTC", :Ethereum=>"ETH", :EOS=>"EOS", :XRP=>"XRP", :ZCash=>"ZEC"}
-    @top_five = CryptoCompareService.new.call_top5_traded
+    @top_five = crypto_service.call_top5_traded
   end
 
   def latest_news
-    @latest_news = CryptoCompareService.new.call_latest_news
+    @latest_news = crypto_service.call_latest_news
+  end
+
+  def crypto_service
+    @crypto_service ||= CryptoCompareService.new
   end
 end
