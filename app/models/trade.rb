c669a4845e64553ffb3abf75bcbef6ba73515097
Nick Petzold <nickpetzold@gmail.com>
2018-12-12 13:03:14 +0000

    Added monetize field to modelsgit add .

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,7 +3,7 @@ class Trade < ApplicationRecord
   belongs_to :cryptocurrency
 
   validates :user_id, :cryptocurrency_id, :fiat_price_cents, :fiat_amount_cents, :cryptocurrency_amount, :transaction_type, presence: true
-  monetize :usd_price_cents, :usd_cents_amount
+  monetize :fiat_price_cents, :fiat_amount_cents
   #                          0      1
   enum transaction_type: [:buy, :sell]
 end
