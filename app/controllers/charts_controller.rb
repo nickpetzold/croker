b6955964be5bfca9ae07d182172a74ca80b88739
Nick Petzold <nickpetzold@gmail.com>
2018-12-12 16:16:45 +0000

    Created a chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,4 +1,15 @@
 class ChartsController < ApplicationController
   def show
+    @cryptocurrency = Cryptocurrency.find(params[:cryptocurrency_id])
+    days = (1..365).to_a
+    prices = crypto_service.call_historical_prices('XMR', days.last)
+    @data = days.zip(prices).first(365)
+  end
+
+  private
+
+  def crypto_service
+    # API MEMOIZATION CODE
+    @crypto_service ||= CryptoCompareService.new
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,10 @@
 //= require Chart.bundle
 //= require chartkick
 import "bootstrap";
+import Chartkick from "chartkick";
+window.Chartkick = Chartkick;
+// for Chart.js
+import Chart from "chart.js";
+Chartkick.addAdapter(Chart);
+Chart.defaults.scale.gridLines.display = false;
+Chart.defaults.scale.xAxes.ticks.display = false;
