fb7334c036d3388ee35122021b1beb1f439a133c
Nick Petzold <nickpetzold@gmail.com>
2018-12-12 15:26:29 +0000

    adding live links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "dashboard", to: "dashboards#dashboard"
-  resources :trades, only: [:index, :show, :new, :create]
+  resources :trades, only: [:index, :show, :new, :create]  do
+    resources :charts, only: :show
+  end
 
   resources :top_ups, only: [:show, :new, :create] do
     resources :payments, only: [:new, :create]
