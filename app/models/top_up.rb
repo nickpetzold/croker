1eb7f2dc7e3761832c66d68acc47bd1629b1ebf8
Nick Petzold <nickpetzold@gmail.com>
2018-12-12 13:09:45 +0000

    added monetize field to models

--- app/models/top_up.rb
+++ app/models/top_up.rb
@@ -2,6 +2,6 @@ class TopUp < ApplicationRecord
   belongs_to :user
 
   validates :user, :fiat_amount_cents, :payment, :transaction_type, :date_of_top_up, presence: true
-
+  monetize :fiat_amount_cents
   enum transaction_type: [:top_up, :withdrawal]
 end
