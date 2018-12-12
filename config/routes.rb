9774d3997f432df04db983ad8224ce7df11e582c
Nick Petzold <nickpetzold@gmail.com>
2018-12-12 11:22:13 +0000

    small change to routes:

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get "dashboards", to: "dashboards#dashboard", as: "dashboard"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "dashboards#dashboard"
   resources :trades, only: [:index, :show, :new, :create]
 end
