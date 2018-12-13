6a8d86137a5f6d8eec09f173bf07d03d4e067343
Ruben Dias <rubenmsd@hotmail.com>
2018-12-13 14:56:29 +0000

    made show render in the index Changer Cryptocurrencies Controller, index and show, to render the show as a partial in the index view, when we select a coin

--- app/controllers/cryptocurrencies_controller.rb
+++ app/controllers/cryptocurrencies_controller.rb
@@ -6,6 +6,13 @@ class CryptocurrenciesController < ApplicationController
     @live_prices = crypto_service.call_current_prices
     # this is an array of hashes with latest news fetched from the api
     @live_news = crypto_service.call_latest_news
+
+    if params["query"].present?
+      @cryptocurrencies = Cryptocurrency.search_by_ticker_name_and_ticker_code(params["query"])
+    else
+      @cryptocurrencies = Cryptocurrency.all
+    end
+
     # this condition takes care of showing the "show" when a cryptocurrency is selected
     if params[:crypto_id]
       @cryptocurrency = Cryptocurrency.find(params[:crypto_id])
@@ -16,18 +23,6 @@ class CryptocurrenciesController < ApplicationController
       # DISPLAY THE TIMEFRAME
     end
   end
-
-  # def show
-  #   @cryptocurrency = Cryptocurrency.find(params[:id])
-  #   @live_price = crypto_service.call_current_prices[@cryptocurrency.ticker_name][@cryptocurrency.ticker_code]
-
-  #   render index
-
-  #   # --------------TODO LATER------------------
-  #   # CALL THE CHART ON THE SHOW
-  #   # CALL THE USER BALANCE ON THE SHOW
-  #   # DISPLAY THE TIMEFRAME
-  # end
 
   def crypto_service
     # API MEMOIZATION CODE
