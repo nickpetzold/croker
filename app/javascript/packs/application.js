e8a9f44fb219e9571aebc4e4bbb1be82c4d97513
Nick Petzold <nickpetzold@gmail.com>
2018-12-13 11:07:18 +0000

    Achieved the format we want with highcharts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,33 @@
 //= require Chart.bundle
-//= require chartkick
+//= require highcharts
 import "bootstrap";
-import Chartkick from "chartkick";
-window.Chartkick = Chartkick;
-// for Chart.js
-// import Chart from "chart.js";
-// Chartkick.addAdapter(Chart);
+import { buildCharts } from '../components/highcharts';
+buildCharts();
 
 
-// for Highcharts
-import Highcharts from "highcharts";
-Chartkick.addAdapter(Highcharts);
-Chart.defaults.global.animation = true;
-// Chart.options.xAxis.labels.enabled = false;
-// Chart.defaults.scale.gridLines.display = false;
 
-// Chart.defaults.global
+
+
+
+
+
+
+
+
+
+
+// import Chartkick from "chartkick";
+// window.Chartkick = Chartkick;
+// // for Chart.js
+// // import Chart from "chart.js";
+// // Chartkick.addAdapter(Chart);
+
+
+// // for Highcharts
+
+// Chartkick.addAdapter(Highcharts);
+// Chart.defaults.global.animation = true;
+// // Chart.options.xAxis.labels.enabled = false;
+// // Chart.defaults.scale.gridLines.display = false;
+
+// // Chart.defaults.global
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "dashboard", to: "dashboards#dashboard"
-  
   resources :cryptocurrencies, only: [:index, :show] do
+    get "call_chart", to: "cryptocurrencies#call_chart"
     resources :trades, only: [:new, :create]
     resources :charts, only: :show
   end
-  
+
   resources :top_ups, only: [:show, :new, :create] do
     resources :payments, only: [:new, :create]
   end
