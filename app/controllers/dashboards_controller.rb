7e7dffbcc3bdf38a68d28660638f234ee5f287bb
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-14 02:19:38 +0000

    had to change controllers a bit in order to facilitate the coding in the html

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,14 @@
 class DashboardsController < ApplicationController
-  before_action :top_five_traded, :latest_news, :top5_winners, :top5_losers
-  before_action :set_portfolio, only: [:dashboard, :portfolio_overview]
+  before_action :set_portfolio, only: [:dashboard, :portfolio_overview, :value_held, :profit_or_loss]
 
   def dashboard
    @portfolio_overview = portfolio_overview
+   @value_held = value_held
+   @profit_or_loss = profit_or_loss
+   @top_five = top_five_traded
+   @top5_winners = top5_winners
+   @top5_losers = top5_losers
+   @latest_news = latest_news
   end
 
   private
@@ -14,26 +19,29 @@ class DashboardsController < ApplicationController
     # ---- THIS IS WHERE WE CALCULATE THE CURRENT PRICE FOR EVERY
     # ---- CRYPTOCURRENCY OWNED BY THE USER
     portfolio_price_hash = {}
-    portfolio_price_hash_of_hashes = {}
-
     @portfolios.each do |portfolio|
-      portfolio_price_hash[portfolio.cryptocurrency.ticker_code] = live_prices[portfolio.cryptocurrency.ticker_name.capitalize][portfolio.cryptocurrency.ticker_code]
-      # {"Bitcoin" => {"BTC"=>3443.67}, "Ethereum" => {"ETH"=>200.04}}
-      portfolio_price_hash_of_hashes[portfolio.cryptocurrency.ticker_name] = portfolio_price_hash
+      live_price = live_prices[tname(portfolio)][tcode(portfolio)]
+      # this will return an hash of hashes like this
+      # {"Ripple"=>{"XRP"=>0.2997}, "Stellar"=>{"XLM"=>0.1036}, "Cardano"=>{"ADA"=>0.02926}, "Bitcoin"=>{"BTC"=>3318.46}}
+      portfolio_price_hash[tname(portfolio)] = { tcode(portfolio) => live_price }
     end
+    portfolio_price_hash
+  end
 
+  def value_held
     # ---- THIS IS WHERE WE CALCULATE THE CURRENT VALUE HELD BY EVERY
     # ---- CRYPTOCURRENCY OWNED BY THE USER
-
     ind_price_crypto_hash = {}
-
     @portfolios.each do |portfolio|
-      live_price_per_crypto = portfolio_price_hash_of_hashes[portfolio.cryptocurrency.ticker_name][portfolio.cryptocurrency.ticker_code]
-      crypto_armound_held = portfolio.crypto_amount_held
+      live_price = @portfolio_overview[tname(portfolio)][tcode(portfolio)]
+      crypto_amount = portfolio.crypto_amount_held
       # this returns something like this {"XRP"=>0.2093e1, "XLM"=>0.521e3, "ADA"=>0.5798e0}
-      ind_price_crypto_hash[portfolio.cryptocurrency.ticker_code] = live_price_per_crypto * crypto_armound_held
+      ind_price_crypto_hash[tcode(portfolio)] = live_price * crypto_amount
     end
+    ind_price_crypto_hash
+  end
 
+  def profit_or_loss
     # ---- THIS IS WHERE WE CALCULATE THE CURRENT
     # ---- THE CURRENT PROFIT / LOSS
 
@@ -43,13 +51,13 @@ class DashboardsController < ApplicationController
     @portfolios.each do |portfolio|
       portfolio.cryptocurrency.trades.buy.each do |trade|
         price_paid = trade.fiat_amount_cents
-        price_now = portfolio_price_hash_of_hashes[trade.cryptocurrency.ticker_name][trade.cryptocurrency.ticker_code] * trade.cryptocurrency_amount
+        price_now = @portfolio_overview[trade.cryptocurrency.ticker_name][trade.cryptocurrency.ticker_code] * trade.cryptocurrency_amount
         profit_or_loss_array << price_now - price_paid
         profit_or_loss_hash_of_arrays[trade.cryptocurrency.ticker_name] = profit_or_loss_array
       end
       portfolio.cryptocurrency.trades.sell.each do |trade|
         price_paid = trade.fiat_amount_cents
-        price_now = portfolio_price_hash_of_hashes[trade.cryptocurrency.ticker_name][trade.cryptocurrency.ticker_code] * trade.cryptocurrency_amount
+        price_now = @portfolio_overview[trade.cryptocurrency.ticker_name][trade.cryptocurrency.ticker_code] * trade.cryptocurrency_amount
         profit_or_loss_array << price_now - price_paid
         profit_or_loss_hash_of_arrays[trade.cryptocurrency.ticker_name] = profit_or_loss_array
       end
@@ -58,6 +66,7 @@ class DashboardsController < ApplicationController
       # "Stellar"=>[-0.999985055e5, -0.3988044e3, -0.11999994022e7, -0.3399487e7, -0.1999994246e6],
       # "Cardano"=>[-0.999985055e5, -0.3988044e3, -0.11999994022e7, -0.3399487e7, -0.1999994246e6]}
     end
+    profit_or_loss_hash_of_arrays
   end
 
   def top_five_traded
@@ -65,24 +74,25 @@ class DashboardsController < ApplicationController
     # across all the different exchanges
     # Also, it comes sorted from the API
     # ex: {:Bitcoin=>"BTC", :Ethereum=>"ETH", :EOS=>"EOS", :XRP=>"XRP", :ZCash=>"ZEC"}
-    @top_five = crypto_service.call_top5_traded
+    crypto_service.call_top5_traded
   end
 
-  def latest_news
-    # This returns an array of hashes with news articles
-    @latest_news = crypto_service.call_latest_news
-  end
 
   def top5_winners
     # this returns an hash like this
     # {"REP"=>8.108108108108116, "WAVES"=>6.535947712418292, "BNB"=>4.228486646884287, "BAT"=>2.183984116479158, "QTUM"=>1.8749999999999878}
-    @top5_winners = crypto_service.call_top5_winners
+    crypto_service.call_top5_winners
   end
 
   def top5_losers
     # this returns an hash like this
     # {"DIG"=>-22.26890756302521, "BCH"=>-4.433786825878439, "ZEC"=>-4.110072323161049, "XLM"=>-3.729401561144838, "LSK"=>-3.361344537815129}
-    @top5_losers = crypto_service.call_top5_losers
+    crypto_service.call_top5_losers
+  end
+
+  def latest_news
+    # This returns an array of hashes with news articles
+    crypto_service.call_latest_news
   end
 
   def crypto_service
@@ -92,5 +102,13 @@ class DashboardsController < ApplicationController
 
   def set_portfolio
     @portfolios = current_user.portfolios
+  end
+
+  def tcode(portfolio)
+    portfolio.cryptocurrency.ticker_code
+  end
+
+  def tname(portfolio)
+    portfolio.cryptocurrency.ticker_name.capitalize
   end
 end
