2911487b8e8e3e105619d5ef27771268f642b055
Anandha Khaou <anandhakhaou@gmail.com>
2018-12-14 15:50:27 +0000

    created the start of landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+      dashboard_path
+  end
+
 end
