9aade6d7ee38fabc4a65dde0af6c0876d824487c
Ruben Dias <rubenmsd@hotmail.com>
2018-12-16 15:10:08 +0000

    hopefully fixed the facebook login...at least on localhost....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
