98c34012ca56313b6c56acb1f5b6e655e7c433e6
filipe custodio <filipe.l.custodio@gmail.com>
2018-12-20 14:21:30 +0000

    Doing the metahelpers for lewagon presentaiton

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
